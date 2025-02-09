5687865b1674a99981f249f21612096fad29db99
Wet Noodle <kluski42@gmail.com>
2025-02-08 22:11:19 -0500

    Using a commit message this time

--- benmo/src/App.js
+++ benmo/src/App.js
@@ -3,11 +3,22 @@ import RenderFoodItem from './foodItem'
 import './App.css';
 
 function App() {
-  const item1 = {
-    name: "Noodles",
-    cost: 15,
-    owner: 1
-  };
+  const items = [
+    {
+      name: "Noodles",
+      cost: 15,
+      owner: -1
+    },
+    {
+      name: "Rice",
+      cost: 5,
+      owner: -1
+    }, {
+      name: "Boba",
+      cost: 7,
+      owner: -1
+    }
+  ];
   return (
     <div>
       <table>
@@ -15,7 +26,9 @@ function App() {
           <th>Item</th>
           <th>Cost</th>
         </tr>
-        <RenderFoodItem foodItem={item1} user={3} />
+        <RenderFoodItem foodItem={items[0]} user={3} />
+        <RenderFoodItem foodItem={items[1]} user={3} />
+        <RenderFoodItem foodItem={items[2]} user={3} />
       </table>
     </div>
   );
